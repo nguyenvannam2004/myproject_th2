ae89c093f94d81daea2906dc57eda04a665e5409
Nam Nguyen <nguyenvannam2004.hd@gmail.com>
2024-12-06 22:47:58 +0700

    Thêm file Category.php vào model

---
+++ tlunews/model/Category.php
@@ -0,0 +1,37 @@
+<<?php
+
+class Category
+{
+    private $id;
+    private $name;
+
+    public function __construct($id, $name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+
+    // Getter methods
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    // Setter methods
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+}
+
+?>
